f62d5273c84a499ff729bfc75055b8e3af91f4c4
wnsgud0428 <wnsgud0428@naver.com>
2023-04-20 13:47:18 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter += 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Click Count",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "$counter",
+              style: const TextStyle(fontSize: 30),
+            ),
+            IconButton(
+              iconSize: 40,
+              onPressed: onClicked,
+              icon: const Icon(
+                Icons.add_box_rounded,
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
